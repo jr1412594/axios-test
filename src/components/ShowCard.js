fa25df367730fccb31abb5af395fc1bf5caf0bfe
jr1412594 <jr.medina1412@gmail.com>
2021-07-31 22:55:10 -0600

    attempt to count likes

--- src/components/ShowCard.js
+++ src/components/ShowCard.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function ShowCard({ tvShow, addToFavs }) {
+
+    const [count, setCount] = useState(0)
+
 
     const handleClick = () => {
         console.log(tvShow,"this was clicked")
         addToFavs(tvShow)
     }
+
+    const likeClick = () => {
+        console.log(count)
+        setCount()
+    }
+
+    
 
     return (
         <div className="tvshow-card" onClick={handleClick}>
@@ -15,7 +25,7 @@ export default function ShowCard({ tvShow, addToFavs }) {
             />
             <div>
             <h1 className="tvshow-title">{tvShow.name}</h1>
-            <button className='show-button'>👍{tvShow.count}</button>
+            <button className='show-button' onClick={ likeClick }>👍: {count}</button>
             </div>
 
         </div>
